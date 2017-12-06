dc249d736d5bf5effa85be9b138f597b0a7319ea
zacksleo <zacksleo@gmail.com>
2017-12-06 16:11:17 +0800

    tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,4 +13,3 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@app', __DIR__);
-Yii::setAlias('@zacksleo\yii2\backend\tests', __DIR__);
