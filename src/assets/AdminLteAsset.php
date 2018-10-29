651cae5a1b4fc5d36e7674f96ba0a40d10bbaf18
zacksleo <zacksleo@gmail.com>
2018-10-29 16:18:59 +0800

    chore(assets): use adminlte

---
+++ src/assets/AdminLteAsset.php
@@ -0,0 +1,43 @@
+<?php
+namespace zacksleo\yii2\backend\assets;
+
+use yii\base\Exception;
+use yii\web\AssetBundle;
+/**
+ * AdminLte AssetBundle
+ * @since 0.1
+ */
+class AdminLteAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/zacksleo/adminlte-assets';
+    public $css = [
+        'css/AdminLTE.min.css',
+    ];
+    public $js = [
+        'js/adminlte.min.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+    /**
+     * @var string|bool Choose skin color, eg. `'skin-blue'` or set `false` to disable skin loading
+     * @see https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
+     */
+    public $skin = '_all-skins';
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        // Append skin color file if specified
+        if ($this->skin) {
+            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
+                throw new Exception('Invalid skin specified');
+            }
+            $this->css[] = sprintf('css/skins/%s.min.css', $this->skin);
+        }
+        parent::init();
+    }
+}
