d62d23e9c8a41e330cec46357f540d75d9f62bfc
zacksleo <zacksleo@gmail.com>
2018-11-08 15:39:59 +0800

    fix(views): my profile views error

--- src/views/my/layouts.php
+++ src/views/my/layouts.php
@@ -2,69 +2,40 @@
 
 use yii\helpers\Html;
 use yii\helpers\Url;
-use zacksleo\yii2\metronic\bundles\pages\ProfileAsset;
 
 $model = $this->params['model'];
-ProfileAsset::register($this);
 ?>
 <?php $this->beginContent('@vendor/zacksleo/yii2-backend/src/views/layouts/layout.php'); ?>
 
 <div class="row">
-    <div class="col-md-12">
-        <!-- BEGIN PROFILE SIDEBAR -->
-        <div class="profile-sidebar">
-            <!-- PORTLET MAIN -->
-            <div class="portlet light profile-sidebar-portlet bordered">
-                <!-- SIDEBAR USERPIC -->
-                <div class="profile-userpic">
-                    <img src="<?= $model->getImageUrl(); ?>" class="img-responsive" alt=""></div>
-                <!-- END SIDEBAR USERPIC -->
-                <!-- SIDEBAR USER TITLE -->
-                <div class="profile-usertitle">
-                    <div class="profile-usertitle-name"> <?= $model->username; ?></div>
-                    <div class="profile-usertitle-job"> <?= $model->name; ?></div>
-                    <br/><br/><br/><br/><br/><br/>
-                </div>
-                <!-- END SIDEBAR USER TITLE -->
-            </div>
-            <!-- END PORTLET MAIN -->
+    <div class="col-md-3">
+        <div class="box box-primary">
+        <div class="box-body box-profile">
+            <img class="profile-user-img img-responsive img-circle" src="<?= $model->getImageUrl(); ?>" alt="User profile picture">
+            <h3 class="profile-username text-center"><?= $model->username; ?></h3>
+            <p class="text-muted text-center"><?= $model->name; ?></p>
         </div>
-        <!-- END BEGIN PROFILE SIDEBAR -->
-        <!-- BEGIN PROFILE CONTENT -->
-        <div class="profile-content">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="portlet light bordered">
-                        <div class="portlet-title tabbable-line">
-                            <div class="caption caption-md">
-                                <i class="icon-globe theme-font hide"></i>
-                                <span class="caption-subject font-blue-madison bold uppercase">账号信息</span>
-                            </div>
-                            <ul class="nav nav-tabs">
-                                <li class="<?= $this->context->action->id == 'profile' ? 'active' : ''; ?>">
-                                    <a href="<?= Url::to(['my/profile']); ?>">个人信息</a>
-                                </li>
-                                <li class="<?= $this->context->action->id == 'avatar' ? 'active' : ''; ?>">
-                                    <a href="<?= Url::to(['my/avatar']); ?>">设置头像</a>
-                                </li>
-                                <li class="<?= $this->context->action->id == 'change-password' ? 'active' : ''; ?>">
-                                    <a href="<?= Url::to(['my/change-password']); ?>">修改密码</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="portlet-body">
-                            <div class="tab-content">
-                                <!-- PERSONAL INFO TAB -->
-                                <div class="tab-pane active" id="tab_1_1">
-                                    <?= $content; ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        </div>
+    </div>
+    <div class="col-md-9">
+        <div class="nav-tabs-custom">
+        <ul class="nav nav-tabs">
+            <li class="<?= $this->context->action->id == 'profile' ? 'active' : ''; ?>">
+                <a href="<?= Url::to(['my/profile']); ?>">个人信息</a>
+            </li>
+            <li class="<?= $this->context->action->id == 'avatar' ? 'active' : ''; ?>">
+                <a href="<?= Url::to(['my/avatar']); ?>">设置头像</a>
+            </li>
+            <li class="<?= $this->context->action->id == 'change-password' ? 'active' : ''; ?>">
+                <a href="<?= Url::to(['my/change-password']); ?>">修改密码</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div class="active tab-pane" id="activity">
+                <?= $content; ?>
             </div>
         </div>
-        <!-- END PROFILE CONTENT -->
+        </div>
     </div>
 </div>
 <?php $this->endContent(); ?>
